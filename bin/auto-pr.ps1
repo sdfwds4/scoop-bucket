99058eb08d785da721cbba9ab7eb6e79f8960121
sdf <sdfwds4@163.com>
2020-07-31 10:49:20 +0800

    modify auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "kodybrown/scoop-nirsoft:master"
+    [String]$upstream = "sdfwds4/scoop-bucket:master"
 )
 
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
